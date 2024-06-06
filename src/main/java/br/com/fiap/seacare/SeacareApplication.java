d76311df6e9390772f67455b99029ce905c715e1
mthaugusto <mthaugusto94@icloud.com>
2024-06-06 08:56:02 -0300

    Sea-care criando projeto

---
+++ src/main/java/br/com/fiap/seacare/SeacareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.seacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/seacare/SeacareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.seacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
